882350b206993a6aef73769ebc2f2cb6f37faf2c
Flyspeck101 <yeetersdeleters12345@gmail.com>
2024-03-26 19:32:57 +1100

    Update copetris.js

--- pages/copetris/copetris.js
+++ pages/copetris/copetris.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 	let tetrisObject = tetris.getObject();
+	let userData = {
+		frames: 60,
+		controls: {
+			cw: "KeyV",
+			ccw: "KeyX",
+			"180": "KeyC",
+			l: "ArrowLeft",
+			r: "ArrowRight",
+			hd: "ArrowUp",
+			sd: "ArrowDown"
+		},
+		handling: {
+			arr: 0,
+			das: 133,
+			sdf: Infinity,
+			dasCancel: true
+		}
+	};
 	(function(){
 		// initialise ui 
 		
@@ -24,14 +42,17 @@ $(document).ready(function(){
 			canvas.height = canvas.offsetHeight;
 			
 			// grid 
-			for (let y = 0; y< tetrisObject.board.field.trueHeight; y++) {
-				for (let x = 0; x < tetrisObject.board.field.width; x++) {
-					let gridSpace = document.createElement("div");
-					const ctx = canvas.getContext("2d");
-					ctx.fillStyle = tetrisObject.meta.colours[tetrisObject.board.field.data[tetrisObject.board.field.trueHeight - tetrisObject.board.field.renderedHeight + y][x]];
-					ctx.fillRect(x * board.clientWidth / tetrisObject.board.field.width + 1, y * board.clientWidth / tetrisObject.board.field.width + 1, board.clientWidth / tetrisObject.board.field.width - 2, board.clientWidth / tetrisObject.board.field.width - 2)
+			function update() {
+				const ctx = canvas.getContext("2d");
+				context.clearRect(0, 0, canvas.width, canvas.height);
+				for (let y = 0; y< tetrisObject.board.field.trueHeight; y++) {
+					for (let x = 0; x < tetrisObject.board.field.width; x++) {
+						ctx.fillStyle = tetrisObject.meta.colours[tetrisObject.board.field.data[tetrisObject.board.field.trueHeight - tetrisObject.board.field.renderedHeight + y][x]];
+						ctx.fillRect(x * board.clientWidth / tetrisObject.board.field.width + 1, y * board.clientWidth / tetrisObject.board.field.width + 1, board.clientWidth / tetrisObject.board.field.width - 2, board.clientWidth / tetrisObject.board.field.width - 2)
+					}
 				}
 			}
+			let render = setInterval(update, 1000/userData.frames);
 		})();
 		
 	})();
